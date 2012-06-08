ee4ddadbb89a36dc7d211fb1c9e4d06bba2e2c2c
Anselm Strauss <strauss@puzzle.ch>
2012-06-08 11:22:59 +0200

    added greater comparisons

---
+++ greater.pp
@@ -0,0 +1,16 @@
+if (2 >= 2) {
+  notice('equal')
+}
+
+if (2 >= 1) {
+  notice('greater')
+}
+
+if (2 >= 3) {
+  notice('smaller')
+}
+
+$a = ''
+if ($a != '') and ('' > 10) {
+  notice('invalid')
+}
