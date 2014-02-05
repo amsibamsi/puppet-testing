2ed6730ce90499cf5a273c1baa6b02b9c6df911a
Anselm Strauss <anselm.strauss@mobi.ch>
2014-02-05 10:08:43 +0100

    added array set perf test

--- cljperf/src/cljperf/core.clj
+++ cljperf/src/cljperf/core.clj
@@ -7,6 +7,9 @@
 (def default-repetitions
   "5")
 
+(def default-size
+  "1000000")
+
 (defn loop-fn
   [iterations]
   (loop [i iterations]
@@ -31,10 +34,9 @@
   (dotimes [n i]
     n))
 
-(defn perf
+(defn perf-loop
   [iterations repetitions]
   (do
-    (println "clojure")
     (println "loop fn...")
     (dotimes [_ repetitions] (time (loop-fn iterations)))
     (println "loop defn...")
@@ -44,13 +46,31 @@
     (println "loop timesdo...")
     (dotimes [_ repetitions] (time (loop-timesdo iterations)))))
 
+(defn perf-array
+  [size repetitions]
+    (do
+      (println "array set ...")
+      (dotimes [_ repetitions]
+        (time
+          (let [a (int-array size 0)]
+            (dotimes [i size] (aset a i i)))))))
+
+(defn perf
+  [iterations size repetitions]
+  (do
+    (println "clojure performance ...")
+    (perf-loop iterations repetitions)
+    (perf-array size repetitions)))
+
 (defn -main
   ([] (-main default-iterations))
-  ([iterations] (-main iterations default-repetitions))
-  ([iterations repetitions]
+  ([iterations] (-main iterations default-size default-repetitions))
+  ([iterations size repetitions]
    (let [i (read-string iterations)
+         s (read-string size)
          r (read-string repetitions)]
      (println i "iterations")
+     (println "size" s)
      (println r "repetitions")
-     (perf i r)
-     (cljperf.Perf/perf i r))))
+     (perf i s r)
+     (cljperf.Perf/perf i s r))))
--- cljperf/src/java/cljperf/Perf.java
+++ cljperf/src/java/cljperf/Perf.java
@@ -2,8 +2,13 @@ package cljperf;
 
 public class Perf {
 
-  public static void perf(long iterations, long repetitions) {
-    System.out.println("java performance");
+  public static void perf(long iterations, long size, long repetitions) {
+    System.out.println("java performance ...");
+    Perf.perf_loop(iterations, repetitions);
+    Perf.perf_array(size, repetitions);
+  }
+
+  public static void perf_loop(long iterations, long repetitions) {
     System.out.println("for loop...");
     for (long i=0; i<repetitions; i++) {
       long before = System.nanoTime();
@@ -11,6 +16,19 @@ public class Perf {
       for (long j=0; j<iterations; j++) {
         // need to do something so loop is not removed for optimization?
         x = x + 1;
+      }
+      double elapsed = ((double)(System.nanoTime() - before)) / 1000000.0;
+      System.out.println("\"Elapsed time: " + elapsed + " msecs\"");
+    }
+  }
+
+  public static void perf_array(long size, long repetitions) {
+    System.out.println("array set ...");
+    for (long i=0; i<repetitions; i++) {
+      long before = System.nanoTime();
+      long[] a = new long[(int)size];
+      for (int j=0; j<(int)size; j++) {
+        a[j] = j;
       }
       double elapsed = ((double)(System.nanoTime() - before)) / 1000000.0;
       System.out.println("\"Elapsed time: " + elapsed + " msecs\"");
